e87d7037098cfda04430b328b1c4806b721e1a15
Iaan Krynauw <iaankrynauw@gmail.com>
2016-04-21 15:53:05 +0200

    [Spinner] Replaced spinner with a circular spinner similar to ttrumpet's.

--- app/components/spinner/spinner.html.ts
+++ app/components/spinner/spinner.html.ts
@@ -1,10 +1,16 @@
   export const htmlSpinner = `
-
-<div class="spinner" [hidden]="!isDelayedRunning">
-  <div class="rect1"></div>
-  <div class="rect2"></div>
-  <div class="rect3"></div>
-  <div class="rect4"></div>
-  <div class="rect5"></div>
+<div class="sk-fading-circle" [hidden]="!isDelayedRunning">
+  <div class="sk-circle1 sk-circle"></div>
+  <div class="sk-circle2 sk-circle"></div>
+  <div class="sk-circle3 sk-circle"></div>
+  <div class="sk-circle4 sk-circle"></div>
+  <div class="sk-circle5 sk-circle"></div>
+  <div class="sk-circle6 sk-circle"></div>
+  <div class="sk-circle7 sk-circle"></div>
+  <div class="sk-circle8 sk-circle"></div>
+  <div class="sk-circle9 sk-circle"></div>
+  <div class="sk-circle10 sk-circle"></div>
+  <div class="sk-circle11 sk-circle"></div>
+  <div class="sk-circle12 sk-circle"></div>
 </div>
 `;
